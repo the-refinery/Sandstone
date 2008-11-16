b016e961c3a58f633cc728965eec37c33d55cfc5
Joshua Walsh <jwalsh@designinginteractive.com>
2008-11-16 15:06:38 -0500

    AJAX Refactoring

--- 4.0/javascript/application.js
+++ 4.0/javascript/application.js
@@ -1,9 +1,3 @@
-function FullFormAJAX(Target, Method, FormID)
-{
-	var FormParameters = Form.serialize(FormID);
-	RaiseAJAXevent(Target, Method, FormParameters);
-}
-
 function RaiseAJAXevent(Target, Method, PostParameters)
 {
 	PostParameters = PostParameters + "&target=" + Target + "&method=" + Method;
