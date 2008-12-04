1bedb95905cf02beae76a7eb2531b43374d69202
Dave Goerlich <dgoerlich@designinginteractive.com>
2008-12-04 13:57:34 -0500

    ChartBase, ChartDataSeries development, PieChart conversion

---
+++ 4.0/codebase/functionalclasses/chart/chartbase.class.php
@@ -0,0 +1,272 @@
+<?php
+
+Namespace::Using("Sandstone.Utilities.Color");
+
+class ChartBase extends Module
+{
+	const BASEURL = 'http://chart.apis.google.com/chart?';
+
+	const EXTENDED_DATA_ENCODING = 0;
+	const SIMPLE_DATA_ENCODING = 1;
+
+	protected $_dataSeries;
+	protected $_title;
+	protected $_width;
+	protected $_height;
+	protected $_scaleMinimumValue;
+	protected $_scaleMaximumValue;
+
+	protected $_dataCount;
+	protected $_minimumDataValue;
+	protected $_maximumDataValue;
+
+	protected $_extendedCodeTable;
+	protected $_simpleCodeTable;
+
+	protected $_urlQueryParameters;
+
+	public function __construct()
+	{
+		$this->_dataSeries = new DIarray();
+
+		//Setup the defaults
+		$this->_scaleMinimumValue = 0;
+
+		$this->SetupCodeTables();
+	}
+
+	/*
+	DataSeries property
+
+	@return DIarray
+	 */
+	public function getDataSeries()
+	{
+		return $this->_dataSeries;
+	}
+
+	/*
+	Title property
+
+	@return string
+	@param string $Value
+	 */
+	public function getTitle()
+	{
+		return $this->_title;
+	}
+
+	public function setTitle($Value)
+	{
+		$this->_title = $Value;
+	}
+
+	/*
+	Width property
+
+	@return integer
+	@param integer $Value
+	 */
+	public function getWidth()
+	{
+		return $this->_width;
+	}
+
+	public function setWidth($Value)
+	{
+		$this->_width = $Value;
+	}
+
+	/*
+	Height property
+
+	@return integer
+	@param integer $Value
+	 */
+	public function getHeight()
+	{
+		return $this->_height;
+	}
+
+	public function setHeight($Value)
+	{
+		$this->_height = $Value;
+	}
+
+	/*
+	ScaleMinimumValue property
+
+	@return decimal
+	@param decimal $Value
+	 */
+	public function getScaleMinimumValue()
+	{
+		return $this->_scaleMinimumValue;
+	}
+
+	public function setScaleMinimumValue($Value)
+	{
+		$this->_scaleMinimumValue = $Value;
+	}
+
+	/*
+	ScaleMaximumValue property
+
+	@return decimal
+	@param decimal $Value
+	 */
+	public function getScaleMaximumValue()
+	{
+		return $this->_scaleMaximumValue;
+	}
+
+	public function setScaleMaximumValue($Value)
+	{
+		$this->_scaleMaximumValue = $Value;
+	}
+
+	/*
+	ExtendedCodeTable property
+
+	@return DIarray
+	 */
+	public function getExtendedCodeTable()
+	{
+		return $this->_extendedCodeTable;
+	}
+
+	/*
+	SimpleCodeTable property
+
+	@return DIarray
+	 */
+	public function getSimpleCodeTable()
+	{
+		return $this->_simpleCodeTable;
+	}
+
+	protected function SetupCodeTables()
+	{
+		//SimpleCodeTable
+		$simpleCodeCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+		$this->_simpleCodeTable = new DIarray(str_split($simpleCodeCharacters));
+
+		//ExtendedCodeTable
+		$extendedCodeCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-.';
+		$extendedCharacters = str_split($extendedCodeCharacters);
+
+		$this->_extendedCodeTable = new DIarray();
+
+		$i = 0;
+		foreach($extendedCharacters as $firstCharacter)
+		{
+
+			foreach($extendedCharacters as $secondCharacter)
+			{
+				$this->_extendedCodeTable[$i] = $firstCharacter . $secondCharacter;
+				$i++;
+			}
+		}
+	}
+
+	public function AddDataSeries($Series)
+	{
+		$returnValue = false;
+
+		if ($Series instanceof ChartDataSeries)
+		{
+			$Series->Chart = $this;
+			$this->_dataSeries[] = $Series;
+
+			$this->_dataCount += $Series->DataCount;
+
+			if (is_set($this->_minimumDataValue) == false || $Series->MinimumDataValue < $this->_minimumDataValue)
+			{
+				$this->_minimumDataValue = $Series->MinimumDataValue;
+			}
+
+			if (is_set($this->_maximumDataValue) == false || $Series->MaximumDataValue > $this->_maximumDataValue)
+			{
+				$this->_maximumDataValue = $Series->MaximumDataValue;
+			}
+
+			$returnValue = true;
+		}
+
+		return $returnValue;
+	}
+
+	protected function EncodeData()
+	{
+		$returnValue = "chd=";
+
+		if ($this->_dataCount > 50)
+		{
+			$mode = ChartBase::SIMPLE_DATA_ENCODING;
+			$returnValue .= "s:";
+		}
+		else
+		{
+			$mode = ChartBase::EXTENDED_DATA_ENCODING;
+			$returnValue .= "e:";
+		}
+
+		if (is_set($this->_scaleMinimumValue))
+		{
+			$encodeMinValue = $this->_scaleMinimumValue;
+		}
+		else
+		{
+			$encodeMinValue = $this->_minimumDataValue;
+		}
+
+		if (is_set($this->_scaleMaximumValue))
+		{
+			$encodeMaxValue = $this->_scaleMaximumValue;
+		}
+		else
+		{
+			$encodeMaxValue = $this->_maximumDataValue;
+		}
+
+		foreach ($this->_dataSeries as $tempSeries)
+		{
+			$encodedData[] = $tempSeries->EncodeData($encodeMinValue, $encodeMaxValue, $mode);
+		}
+
+		$returnValue .= implode(",", $encodedData);
+
+		return $returnValue;
+	}
+
+	protected function SetupURLqueryParameters()
+	{
+		$this->_urlQueryParameters[] = "chtt=" . urlencode($this->_title);
+		$this->_urlQueryParameters[] = "chs={$this->_width}x{$this->_height}";
+
+		$this->_urlQueryParameters[] = $this->EncodeData();
+	}
+
+	public function BuildURL()
+	{
+		$this->_urlQueryParameters = Array();
+
+		$this->SetupURLqueryParameters();
+
+		$returnValue = ChartBase::BASEURL . implode("&", $this->_urlQueryParameters);
+
+		return $returnValue;
+	}
+
+	public function OutputChart()
+	{
+		$url = $this->BuildURL();
+
+		$fileContents = file_get_contents($url);
+
+        Header("Content-Type: image/png");
+		echo $fileContents;
+	}
+
+}
+?>
--- 4.0/codebase/functionalclasses/chart/piechart.class.php
+++ 4.0/codebase/functionalclasses/chart/piechart.class.php
@@ -1,56 +1,69 @@
 <?php
 
-class PieChart extends GoogleChart
+class PieChart extends ChartBase
 {
-	protected $_is3D = false;
-	protected $_labels = array();
-	protected $_isUsingLabels = false;
-	
-	protected function BuildParameters()
+	protected $_is3D;
+	protected $_colors;
+
+	public function __construct()
 	{
-		$returnValue = array(
-								'cht' => ($this->_is3D) ? 'p3' : 'p',
-								'chl' => $this->Labels
-							);
-		
-		return $returnValue;
+		parent::__construct();
+
+		$this->_colors = Array();
 	}
-	
-	public function AddData($data, $label = null)
-	{
-		// If using labels, ALL data must have a label
-		if ($label)
-		{
-			$this->_isUsingLabels = true;
-			$this->_labels[] = $label;
-		}
-		
-		$this->_data[] = $data;
-	}
-	
-	public function getLabels()
-	{
-		if ($this->_isUsingLabels)
-		{
-			$returnValue = implode("|", $this->_labels);
-		}
-		else
-		{
-			$returnValue = null;
-		}
-		
-		return $returnValue; 
-	}
-	
+
 	protected function getIs3D()
 	{
 		return $this->_is3D;
 	}
-	
+
 	protected function setIs3D($Value)
 	{
-		$this->_is3D = true;
+		$this->_is3D = $Value;
 	}
+
+	public function AddColor($Color)
+	{
+		if (is_array($Color) || $Color instanceof DIarray)
+		{
+			$this->_colors = Array();
+
+			foreach ($Color as $tempColor)
+			{
+				$this->_colors[] = $tempColor;
+			}
+		}
+		else
+		{
+			$this->_colors[] = $Color;
+		}
+	}
+
+	protected function SetupURLqueryParameters()
+	{
+
+		parent::SetupURLqueryParameters();
+
+		if ($this->_is3D)
+		{
+			$this->_urlQueryParameters[] = "cht=p3";
+		}
+		else
+		{
+			$this->_urlQueryParameters[] = "cht=p";
+		}
+
+		if (count($this->_dataSeries[0]->Labels) > 0)
+		{
+			$this->_urlQueryParameters[] = "chl=" . $this->_dataSeries[0]->FormattedLabels;
+		}
+
+		if (count($this->_colors)  > 0)
+		{
+			$this->_urlQueryParameters[] = "chco=" . implode("|", $this->_colors);
+		}
+	}
+
 }
 
 ?>
