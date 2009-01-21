b8c86ee19d837a6ca53bc18334151b0910774257
Dave Goerlich <dgoerlich@bigboy.(none)>
2009-01-21 11:09:03 -0500

    Entity Messages page

--- 4.0/codebase/functionalclasses/application/pages/base/entitymessagesbase.page.php
+++ 4.0/codebase/functionalclasses/application/pages/base/entitymessagesbase.page.php
@@ -6,7 +6,35 @@ NameSpace::Using("Sandstone.Message");
 class EntityMessagesBasePage extends BasePage
 {
 
-    protected $_messages;
+    protected $_entity;
+    
+	protected function BuildControlArray($EventParameters)
+	{
+        
+		$this->MessageList = new RepeaterControl();
+		$this->MessageList->ItemIDsuffixFormat = "{MessageID}";
+
+        $this->AddMessageForm = new PageForm($EventParameters);
+
+        $this->AddMessageForm->Title = new TextBoxControl();
+        $this->AddMessageForm->Title->LabelText = "Title";
+        $this->AddMessageForm->Title->AddValidator("GenericValidator", "IsRequired");
+        
+        $this->AddMessageForm->Content = new TextAreaControl();
+        $this->AddMessageForm->Content->LabelText = "Message";
+        $this->AddMessageForm->Content->AddValidator("GenericValidator", "IsRequired");
+        
+        $this->AddMessageForm->Submit = new SubmitButtonControl();
+		
+        parent::BuildControlArray($EventParameters);
+    }
+    
+    
+	protected function LoadControlData($EventParameters)
+	{
+        $this->MessageList->Data = $this->_entity->Messages->Messages;
+    }
+    
     
 }
 ?>
