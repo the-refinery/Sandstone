eee168d747064881e78cf5e76607316955bfdb3b
Dave Goerlich <dgoerlich@bigboy.(none)>
2009-02-23 16:19:55 -0500

    Widget framework

---
+++ 4.0/codebase/functionalclasses/application/widgets/graphicwidget.class.php
@@ -0,0 +1,20 @@
+<?php
+/*
+Graphic Widget Class File
+
+@package Sandstone
+@subpackage Application
+*/
+
+Namespace::Using("Sandstone.Chart");
+
+class GraphicWidget extends WidgetBase
+{
+
+    public function OutputPNG()
+    {
+        
+    }
+
+}
+?>
