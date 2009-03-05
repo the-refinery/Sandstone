8fc8fc5e0f4d3b40d6d849a11cffb372e64beefb
Joshua Walsh <jwalsh@designinginteractive.com>
2009-03-05 14:25:40 -0500

    Title Textbox

--- 4.0/javascript/controls.js
+++ 4.0/javascript/controls.js
@@ -42,61 +42,42 @@ function SetControlMessage(ControlName, MessageText)
 		}
 	}
 }
-// 
-// function SelectDropdownItem(DomID, Value)
-// {
-//     $(DomID)[0].selected = true;
-// 
-// 	var i;
-//     for (i = 0; i < $(DomID).length; i++)
-//     {
-//         if ( $(DomID)[i].value == Value )
-//         {
-//             $(DomID)[i].selected = true;
-//         }
-//     }
-// }
 
-// === Title Textbox ===
+function SelectDropdownItem(DomID, Value)
+{
+    $("#"+ DomID + " option[value='" + Value + "']").attr('selected', 'selected');
+}
 
-// document.observe('dom:loaded',function()
-// {
-// 	// Find all titletextboxes by looking for class name
-// 	$$(".titletextbox_body").each(function(element) 
-// 	{
-// 		if ($F(element) == '')
-// 		{
-// 			element.value = $(element.id + '_Label').innerHTML;
-// 			element.addClassName('titletextbox_blank');
-// 		}
-// 		$(element.id + '_Label').hide();
-// 		
-// 		element.observe("focus",function(event)
-// 		{
-// 			var target = event.element();
-// 
-// 			if ($F(target) == $(target.id + '_Label').innerHTML)
-// 			{
-// 				target.removeClassName('titletextbox_blank');
-// 				target.value = '';
-// 			}
-// 
-// 			target.fire("controls:titletextbox:focus:callback");
-// 	  	});
-// 	
-// 		element.observe("blur", function(event)
-// 		{
-// 			var target = event.element();
-// 
-// 			if ($F(target) == '')
-// 			{
-// 				target.addClassName('titletextbox_blank');
-// 				target.value = $(target.id + '_Label').innerHTML;
-// 			}
-// 
-// 			target.fire("controls:titletextbox:blur:callback");
-// 		});
-// 	});
-// });
-// 
-// 
+$(document).ready(function()
+{
+	// === Title Textbox ===
+	$("input.titletextbox_body").each(function() 
+	{
+		if ($(this).val() == '')
+		{
+			$(this).val($('#' + $(this).attr('id') + '_Label').html());
+			$(this).addClass('titletextbox_blank');
+		}
+		$('#' + $(this).attr('id') + '_Label').hide();
+		
+		$(this).bind("focus",function(event)
+		{
+			if ($(this).val() == $('#' + $(this).attr('id') + '_Label').html())
+			{
+				$(this).removeClass('titletextbox_blank');
+				$(this).val('');
+			}
+	  	});
+	
+		$(this).bind("blur", function(event)
+		{
+			if ($(this).val() == '')
+			{
+				$(this).addClass('titletextbox_blank');
+				$(this).val($('#' + $(this).attr('id') + '_Label').html());
+			}
+		});
+	});
+});
+
+
