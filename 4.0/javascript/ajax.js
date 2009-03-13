93414bdd4f0fce2a3a2e65477772a1d53f834a18
Joshua Walsh <jwalsh@designinginteractive.com>
2009-03-13 15:22:24 -0400

    AJAX Post can accept query string parameters

--- 4.0/javascript/ajax.js
+++ 4.0/javascript/ajax.js
@@ -1,7 +1,9 @@
-function RaiseAJAXevent(Target, Method, PostParameters)
+function RaiseAJAXevent(Target, Method, PostParameters, GetParameters)
 {
+	// PostParameters = Hash
+	// Get Parameters = Query String
 	PostParameters = $.extend({'target':Target, 'method':Method}, PostParameters);
-	
-	$.post(AJAX_URL, PostParameters, null, "script");	
+		
+	$.post(AJAX_URL+'?'+GetParameters, PostParameters, null, "script");	
 }
 
