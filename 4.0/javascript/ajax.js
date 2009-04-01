daad2a24ff128985a912174356f966858ea3cc88
Josh Walsh <jwalsh@designinginteractive.com>
2009-04-01 17:29:32 -0400

    Fix for Ajax Null Get Parameters

--- 4.0/javascript/ajax.js
+++ 4.0/javascript/ajax.js
@@ -3,7 +3,12 @@ function RaiseAJAXevent(Target, Method, PostParameters, GetParameters)
 	// PostParameters = Hash
 	// Get Parameters = Query String
 	PostParameters = $.extend({'target':Target, 'method':Method}, PostParameters);
-		
+	
+	if (GetParameters == null)
+	{
+		GetParameters = '';
+	}
+	
 	$.post(AJAX_URL+'?'+GetParameters, PostParameters, null, "script");	
 }
 
