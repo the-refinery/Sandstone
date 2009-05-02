49d5c69021dc5d4a5092a787f29cbad1c0485bb0
Dave Goerlich <dgoerlich@superbird.(none)>
2009-05-02 15:00:48 -0400

    DEV

--- 4.0/codebase/entityclasses/creditcard/transaction/capturetransaction.class.php
+++ 4.0/codebase/entityclasses/creditcard/transaction/capturetransaction.class.php
@@ -11,6 +11,33 @@ class CaptureTransaction extends BaseCreditCardTransaction
 {
 	public function ProcessCredit($Amount = null)
 	{
+		if (is_set($this->_cimCustomerProfileID))
+		{
+			$returnValue = $this->ProcessCIMtransaction($Amount);
+		}
+		else
+		{
+			$returnValue = $this->ProcessMerchantAccountTransaction($Amount);
+		}
+
+		return $returnValue;
+	}
+
+	protected function ProcessCIMtransaction($Amount)
+	{
+
+		$customerProfile = new CIMcustomerProfile($this->_cimCustomerProfileID);
+
+		if ($customerProfile->PaymentProfile->PaymentProfileID == $this->_cimPaymentProfileID)
+		{
+			$returnValue = $customerProfile->PaymentProfile->ProcessCredit($this, $Amount);
+		}
+
+		return $returnValue;
+	}
+
+	protected function ProcessMerchantAccountTransaction($Amount)
+	{
 		$merchantAccount = Application::License()->ActiveMerchantAccount;
 		$returnValue = $merchantAccount->ProcessCredit($this, $Amount);
 
