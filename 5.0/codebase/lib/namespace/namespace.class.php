ccf710eabf33623e770bd0aaaa31e4d1bcddc0eb
Dave Goerlich <dgoerlich@superbird.(none)>
2009-05-17 18:49:13 -0400

    Namespace including single files

--- 5.0/codebase/lib/namespace/namespace.class.php
+++ 5.0/codebase/lib/namespace/namespace.class.php
@@ -2,9 +2,37 @@
 
 class Namespace
 {
-
-	static public function Using($Namespace)
+	protected function __construct()
 	{
 
 	}
+
+	static public function Instance()
+	{
+		static $nameSpace;
+
+		if (isset($nameSpace) == false)
+		{
+			$nameSpace = new NameSpace();
+		}
+
+		return $nameSpace;
+	}
+
+	static public function Using($Namespace)
+	{
+		$Namespace = "codebase/" . $Namespace;
+
+		require_once($Namespace);
+	}
+
+	static public function AutoLoad($ClassName)
+	{
+
+	}
+}
+
+function __autoload($ClassName)
+{
+	Namespace::AutoLoad($ClassName);
 }
