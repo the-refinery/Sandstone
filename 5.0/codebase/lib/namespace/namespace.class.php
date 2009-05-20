7c252f8da2b155c63e74a10880a50cc2aa2a90ca
Dave Goerlich <dgoerlich@bigboy.(none)>
2009-05-19 22:05:26 -0400

    Namespace development

--- 5.0/codebase/lib/namespace/namespace.class.php
+++ 5.0/codebase/lib/namespace/namespace.class.php
@@ -2,9 +2,13 @@
 
 class Namespace
 {
+	protected $_includedClasses;
+	protected $_usedNamespaces;
+
 	protected function __construct()
 	{
-
+		$this->_includedClasses = Array();
+		$this->_usedNamespaces = Array();
 	}
 
 	static public function Instance()
@@ -21,21 +25,76 @@ class Namespace
 
 	static public function Using($Namespace)
 	{
-		$BasePath = "codebase/" . $Namespace;
+		$ns = Namespace::Instance();
 
-		$parser = new ParseADirectory();
-
-		$files = $parser->FindFilesInADirectory($BasePath . "*.*.php");
-
-		foreach ($files as $tempFile)
-		{
-			require_once($tempFile);
-		}
+		$ns->ProcessUsing($Namespace);
 	}
 
 	static public function AutoLoad($ClassName)
 	{
+		$ns = Namespace::Instance();
 
+		$ns->ProcessAutoLoad($ClassName);
+	}
+
+	public function ProcessUsing($Namespace)
+	{
+		if (array_key_exists(strtolower($Namespace), $this->_usedNamespaces) == false)
+		{
+			$namespaceDirectory = $this->GenerateBasePathFromNamespace($Namespace);
+
+			$parser = new ParseADirectory();
+
+			$files = $parser->FindFilesInADirectory($namespaceDirectory . "*.*.php");
+
+			foreach ($files as $tempFile)
+			{
+				$this->ProcessFile($tempFile);
+			}
+
+			$this->_usedNamespaces[$Namespace] = $namespaceDirectory;
+		}
+
+	}
+
+	protected function GenerateBasePathFromNamespace($Namespace)
+	{
+		
+		$returnValue = strtolower($Namespace);
+
+		$returnValue = str_replace(".", "/", $returnValue);
+
+		$returnValue = "codebase/" . $returnValue . "/";
+
+		return $returnValue;
+	}
+
+	protected function ProcessFile($FileSpec)
+	{
+		$className = $this->ParseClassName($FileSpec);
+
+		$this->_includedClasses[$className] = $FileSpec;
+	}
+
+	protected function ParseClassName($FileSpec)
+	{
+		$fileNameStart = strrpos($FileSpec, "/") + 1;
+		$fileName = substr($FileSpec, $fileNameStart);
+
+		$classNameEnd = strpos($fileName, ".");
+		$returnValue = strtolower(substr($fileName, 0, $classNameEnd));
+
+		return $returnValue;
+	}
+
+	public function ProcessAutoLoad($ClassName)
+	{
+		$ClassName = strtolower($ClassName);
+
+		if (array_key_exists($ClassName, $this->_includedClasses))
+		{
+			require_once($this->_includedClasses[$ClassName]);
+		}
 	}
 }
 
