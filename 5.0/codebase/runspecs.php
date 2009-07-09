4aff818df8e9ff7c060806022c9fae2cc6400f6c
Joshua Walsh <jwalsh@designinginteractive.com>
2009-07-09 17:00:09 -0400

    Console spec runner now uses all specs in current namespace

--- 5.0/codebase/runspecs.php
+++ 5.0/codebase/runspecs.php
@@ -3,17 +3,14 @@
 include_once('setupspecs.php');
 
 $SpecRunner = new RunSpecsInBash();
-$SpecRunner->DescribeBehavior('FormatStringSpec');
-$SpecRunner->DescribeBehavior('OutputToBashSpec');
-$SpecRunner->DescribeBehavior('DescribeBehaviorSpec');
-$SpecRunner->DescribeBehavior('RunSpecsSpec');
-$SpecRunner->DescribeBehavior('AssertConditionSpec');
-$SpecRunner->DescribeBehavior('MockSpec');
-$SpecRunner->DescribeBehavior('AlterClassSpec');
-$SpecRunner->DescribeBehavior('componentspec');
-$SpecRunner->DescribeBehavior('NamespaceSpec');
-$SpecRunner->DescribeBehavior('ParseADirectorySpec');
-$SpecRunner->DescribeBehavior('BenchmarkSpec');
-$SpecRunner->DescribeBehavior('RouteSpec');
-$SpecRunner->DescribeBehavior('RoutingSpec');
+
+$classes = Namespace::Classes();
+foreach ($classes as $tempClass)
+{
+	if (strlen($tempClass) - stripos($tempClass,'spec') == 4)
+	{
+		$SpecRunner->DescribeBehavior($tempClass);
+	}
+}
+
 $SpecRunner->Run();
