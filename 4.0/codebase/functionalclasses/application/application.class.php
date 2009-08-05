9a409397c6eccb0427e1b86f6bcd0c3e5339f200
Dave Goerlich <dgoerlich@bigboy.(none)>
2009-08-05 15:56:41 -0400

    Attempt to fix ARG's login

--- 4.0/codebase/functionalclasses/application/application.class.php
+++ 4.0/codebase/functionalclasses/application/application.class.php
@@ -749,9 +749,12 @@ class Application extends Module
 			
 			$domain = str_replace("http://", "", Application::BaseURL());
 
-			if (substr($domain,-1,1) == "/")
+			if (substr_count($domain, "/") == 1)
 			{
-				$domain = substr($domain,0,-1);
+				if (substr($domain,-1,1) == "/")
+				{
+					$domain = substr($domain,0,-1);
+				}
 			}
 
 			setcookie($Name, $Value, $expires, "/", $domain);
